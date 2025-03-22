30415b29d1a99d264e8e7967cf2465584b5dff2e
Christopher Weaver <crweaver225@yahoo.com>
2025-03-22 07:15:46 -0500

    quicksort

---
+++ QuickSort/main.hs
@@ -0,0 +1,10 @@
+
+quicksort :: [Int] -> [Int]
+quicksort [] = []
+quicksort (x:xs) = (quicksort lesser) ++ [x] ++ (quicksort greater)
+  where
+    lesser = filter (< x) xs
+    greater = filter (>= x) xs
+
+main :: IO ()
+main = print $ quicksort [5,3,1,2,9,6,4]
