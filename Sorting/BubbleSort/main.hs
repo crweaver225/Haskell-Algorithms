4d80e81c6f9adf532501dab572a42f8fa9021add
Christopher Weaver <crweaver225@yahoo.com>
2025-03-25 14:30:56 -0500

    optimized bubble sort

--- Sorting/BubbleSort/main.hs
+++ Sorting/BubbleSort/main.hs
@@ -10,5 +10,5 @@ bubblePass :: (Ord a) => [a] -> [a]
 bubblePass [] = []
 bubblePass [x] = [x]
 bubblePass (x1:x2:xs)
-    | x1 > x2 = [x2] ++ (bubblePass ([x1] ++ xs))
-    | otherwise = [x1] ++ (bubblePass ([x2] ++ xs))
+    | x1 > x2 = x2 : bubblePass (x1:xs)
+    | otherwise = x1 : bubblePass (x2:xs)
